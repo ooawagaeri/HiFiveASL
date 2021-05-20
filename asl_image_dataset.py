64697e1df64fef23ac8c55d8a04039b90745d0e3
ooawagaeri <oastreach@gmail.com>
2021-05-20 18:56:48 +0800

    Base tensor training model

---
+++ asl_image_dataset.py
@@ -0,0 +1,33 @@
+"""
+asl_image_dataset.py
+Used to augment dataset images in order to better CNN performance
+"""
+
+
+import albumentations
+import cv2
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+
+class ASLImageDataset(Dataset):
+    def __init__(self, path, labels):
+        self.X = path
+        self.y = labels
+        # Augmentation pipeline; Resize
+        self.aug = albumentations.Compose([
+            albumentations.Resize(224, 224, always_apply=True),
+        ])
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, i):
+        image = cv2.imread(self.X[i])
+        # Perform augmentation on image
+        image = self.aug(image=np.array(image))['image']
+        image = np.transpose(image, (2, 0, 1)).astype(np.float32)
+        label = self.y[i]
+        # Create tensors
+        return torch.tensor(image, dtype=torch.float), torch.tensor(label, dtype=torch.long)
---
+++ create_csv.py
@@ -0,0 +1,61 @@
+"""
+create_csv.py
+Used to map preprocessed dataset of images to alphabet type as CSV and pkl format
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import joblib
+from sklearn.preprocessing import LabelBinarizer
+from tqdm import tqdm
+from imutils import paths
+
+input_path = 'output/preprocessed_image'
+output_csv = 'output/data_alpha.csv'
+output_pkl = 'output/lb_alpha.pkl'
+
+# Get all subdirectories within pre-processed directory
+preprocessed_paths = list(paths.list_images(input_path))
+
+# DataFrame: Two-dimensional structure to map image to target (value)
+data = pd.DataFrame()
+labels = []
+
+# Iterate through all files
+for index, sub_path in tqdm(enumerate(preprocessed_paths), total=len(preprocessed_paths)):
+    # Get alphabet label
+    label = sub_path.split(os.path.sep)[-2]
+    # Store relative path for mapping image to label
+    data.loc[index, 'sub_path'] = sub_path
+    labels.append(label)
+
+labels = np.array(labels)
+
+# Assigns categorical (alphabet) value to data
+# i.e Given a certain set of data, return a predicted label
+lb = LabelBinarizer()
+# Scale training data and parameters
+labels = lb.fit_transform(labels)
+print(f"First label: {labels[0]}")
+print(f"Mapping first label to respective category: {lb.classes_[0]}")
+print(f"Total number of labels (instances): {len(labels)}")
+
+# Iterate through all labels
+for i in range(len(labels)):
+    # Get index of largest value
+    index = np.argmax(labels[i])
+    # Store label index to index
+    data.loc[i, 'target'] = int(index)
+
+# Shuffles dataset
+data = data.sample(frac=1).reset_index(drop=True)
+
+# Save dataset as CSV file
+print('Saving dataset...')
+data.to_csv(output_csv, index=False)
+
+# Save binarized labels as pickle file
+print('Saving label binarizer...')
+joblib.dump(lb, output_pkl)
+# print(data.head(5))
