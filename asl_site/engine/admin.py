3af612c5ce6eb37f75608c094467afe9f32f1c02
ooawagaeri <oastreach@gmail.com>
2021-05-23 22:03:26 +0800

    Git clean up for restructure

--- asl_site/engine/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import ASL
-
-# Register your models here.
-admin.site.register(ASL)
--- asl_site/engine/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import ASL
-
-
-class ASLSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ASL
-        fields = '__all__'
